296b4ea729e14b546eef7e04c30e135a712fcec3
brayansbaezm <brayansbaezm@INTER-PF3G6L26>
2025-07-23 17:06:36 -0500

    update service with models

--- models/replacement.py
+++ models/replacement.py
@@ -10,15 +10,33 @@ class Replacement(BaseModel):
 
 known_replacements: List[Replacement] = [
     Replacement(
-        part_number="123-ABC",
-        label="0 vaso",
+        part_number="4400454",
+        label="0 Inyector de grasa",
         quantity=11,
-        name="Vaso Ejemplo"
+        name="Inyector de grasa"
     ),
     Replacement(
-        part_number="456-DEF",
-        label="1 estuche",
+        part_number="4400422",
+        label="1 MANIPULADOR IZQUIERDO",
         quantity=12,
-        name="Estuche Ejemplo"
+        name="Manipulador izquierdo"
+    ),
+    Replacement(
+        part_number="4192035",
+        label="2 SENSOR DE NIVEL DE GRASA",
+        quantity=11,
+        name="Sensor de nivel de grasa"
+    ),
+    Replacement(
+        part_number="4400418",
+        label="3 FILTRO DE AIRE ACONDICIONADO",
+        quantity=12,
+        name="Filtro de aire condicionado"
+    ),
+    Replacement(
+        part_number="4385019",
+        label="4 MANGUERA",
+        quantity=11,
+        name="Manguera de alta bomba auxiliar"
     )
 ]
